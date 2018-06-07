3c8c6128f415a5aa14ab7357419a37df5ac7fe72
paigevogel <paigevogel36@gmail.com>
2018-06-07 20:32:15 +0000

    automatically backed up by learn

---
+++ oobasics.rb
@@ -0,0 +1,44 @@
+class Book
+  def initialize(title, author, page_count, genre)
+    @title=title
+    @author=author
+    @page_count=page_count
+    @genre=genre
+  end 
+
+  def title
+    @title
+  end
+  
+  def title=(title)
+    @title = title
+  end
+  
+  def author
+    @author
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def page_count
+    @page_count
+  end 
+  
+  def page_count=(page_count)
+    @page_count=page_count
+  end
+  
+  def genre
+    @genre
+  end
+  
+  def genre=(genre)
+    @genre=genre
+  end
+end 
+
+book1 = Book.new("The Giver", "Lois Lowry", 300, "Fiction")
+book2 = Book.new("Catcher in the Rye", "JD Sailinger", 102, "Literary realism")
+book3 = Book.new("")
